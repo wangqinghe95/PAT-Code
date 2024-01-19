1ecf8ef0014ec93fc4ad8942b2fd518a8a021882
luis.wang <luis.wang@aptiv.com>
2024-01-19 00:56:06 -0500

    [Modify] modify A1103.cpp

--- Advanced-C++/A1103.cpp
+++ Advanced-C++/A1103.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
-
 using namespace std;
 
 vector<int> fac;
 vector<int> ans, temp;
 
-int g_max_fac_sum = -1;
+int g_max_index_sum = -1;
 
 int n,k,p;
 
-int getPower(int base, int power)
+int getPower(int base, int p)
 {
     int ans = 1;
-    for(int i = 0; i < power; ++i) {
+    for(int i = 0; i < p; ++i) {
         ans *= base;
     }
     return ans;
@@ -22,54 +21,52 @@ int getPower(int base, int power)
 void init()
 {
     int tmp = 0;
-    int index = 1;
+    int index = 0;
     while (tmp <= n)
     {
         fac.push_back(tmp);
-        tmp = getPower(index++, p);
+        tmp = getPower(++index, p);
     }
 }
 
-void DFS(int index, int fac_cnt, int n_sum, int fac_sum)
+void DFS(int index, int index_cnt, int fac_sum, int index_sum)
 {
-    if(n_sum == n && fac_cnt == k) {
-        if(fac_sum > g_max_fac_sum) {
-            g_max_fac_sum = fac_sum;
+    if(fac_sum == n && index_cnt == k) {
+        if(index_sum > g_max_index_sum) {
             ans = temp;
+            g_max_index_sum = index_sum;
         }
+        return;
     }
 
-    if(n_sum > n || fac_cnt > k) {
+    if(fac_sum > n || index_cnt > k ) {
         return;
     }
 
     if(index >= 1) {
         temp.push_back(index);
-        DFS(index, fac_cnt+1, n_sum+fac[index], fac_sum+index);
+        DFS(index, index_cnt+1, fac_sum+fac[index], index_sum+index);
         temp.pop_back();
-        DFS(index-1, fac_cnt, n_sum, fac_sum);
+        DFS(index-1, index_cnt, fac_sum, index_sum);
     }
-    
 }
-
 
 int main()
 {
     scanf("%d%d%d",&n, &k, &p);
-
     init();
-    DFS(fac.size()-1, 0, 0, 0);
 
-    if(g_max_fac_sum == -1) {
+    DFS(fac.size()-1, 0,0,0);
+
+    if(g_max_index_sum == -1) {
         printf("Impossible\n");
     }
     else {
-        printf("%d = %d^%d", n, ans[0], p);
+        printf("%d = %d^%d", n, ans[0],p);
         for(int i = 1; i < ans.size(); ++i) {
             printf(" + %d^%d", ans[i], p);
         }
     }
 
     return 0;
-    
 }
